b69f4a8694dde2cf38e0581fbacbf83e2175135e
ayed°FNF° <97188929+ayedbahan@users.noreply.github.com>
2023-01-14 18:50:10 +0100

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -131,7 +131,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Vs Logger v", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Vs Logger", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
